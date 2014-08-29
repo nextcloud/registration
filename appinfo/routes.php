5aefcd63517fb0fef63261b17b5a9107cb8c10e1
Pellaeon Lin <nfsmwlin@gmail.com>
2014-08-29 17:36:00 +0000

    Add all routes

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -22,5 +22,8 @@ namespace OCA\Registration\AppInfo;
 $application = new Application();
 
 $application->registerRoutes($this, array('routes' => array(
-	array('name' => 'registration#displayRegisterPage', 'url' => '/', 'verb' => 'GET')
+	array('name' => 'registration#displayRegisterPage', 'url' => '/', 'verb' => 'GET'),
+	array('name' => 'registration#validateEmail', 'url' => '/', 'verb' => 'POST'),
+	array('name' => 'registration#verifyToken', 'url' => '/verify/{token}', 'verb' => 'GET'),
+	array('name' => 'registration#createAccount', 'url' => '/verify/{token}', 'verb' => 'POST')
 )));
