c48b224b9326138ac0cedf34e3b66583b09bac43
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-01 00:28:12 -0400

    [tx-robot] updated from transifex

---
+++ l10n/et_EE.php
@@ -0,0 +1,16 @@
+<?php
+$TRANSLATIONS = array(
+"Email address you entered is not valid" => "Sinu poolt sisestatud e-posti aadress pole korrektne",
+"There is already a pending registration with this email" => "Selle aadressi pealt juba oodatakse registreerumise kinnitamist",
+"Verify your ownCloud registration request" => "Kinnita oma ownCloudi registreerumise soov",
+"To create a new account on ownCloud, just click the following link: {link}" => "ownCloudi uue kasutajakonto loomisels kliki lihtsalt järgnevale lingile: {link}",
+"Username" => "Kasutajanimi",
+"Password" => "Parool",
+"Create account" => "Loo konto",
+"Thank you for registering, you should receive verification link in a few minutes." => "Täname, et registreerusid. Kinnituslink saadetakse mõne minuti pärast.",
+"Email" => "E-post",
+"Request verification link" => "Küsi kinnituslinki",
+"Please re-enter a valid email address" => "Palun sisesta e-posti aadress uuesti",
+"You will receive an email with verification link" => "Kinnituslink saadetakse sulle e-kirjaga"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
---
+++ l10n/nl.php
@@ -0,0 +1,16 @@
+<?php
+$TRANSLATIONS = array(
+"Email address you entered is not valid" => "Het e-mailadres dat u heeft ingevuld is niet geldig",
+"There is already a pending registration with this email" => "Er is aan een registratie in behandeling met dit e-mailadres",
+"Verify your ownCloud registration request" => "Controleer uw ownCloud registratie verzoek",
+"To create a new account on ownCloud, just click the following link: {link}" => "Klik op de volgende link om een nieuw ownCloud-account te maken: {link}",
+"Username" => "Gebruikersnaam",
+"Password" => "Wachtwoord",
+"Create account" => "Maak account aan",
+"Thank you for registering, you should receive verification link in a few minutes." => "Dank voor uw registratie. U zou binnen een paar minuten een verificatie-link moeten ontvangen.",
+"Email" => "E-mailadres",
+"Request verification link" => "Vraag een verificatie-link",
+"Please re-enter a valid email address" => "Vul opnieuw een geldig e-mailadres in",
+"You will receive an email with verification link" => "U ontvangt een e-mail met een verificatie-link"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
