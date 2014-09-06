d8236efa1d18cfd04c9c5691f11b65b1053132c7
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-06 00:28:09 -0400

    [tx-robot] updated from transifex

---
+++ l10n/pt_PT.php
@@ -0,0 +1,14 @@
+<?php
+$TRANSLATIONS = array(
+"Email address you entered is not valid" => "O endereço de correio electrónico introduzido é inválido",
+"Verify your ownCloud registration request" => "Verifique o seu pedido de registo de ownCloud",
+"Username" => "Nome de utilizador",
+"Password" => "Senha",
+"Create account" => "Criar conta",
+"Thank you for registering, you should receive verification link in a few minutes." => "Obrigado por registar, em alguns minutos você receberá um email de confirmação com um link.",
+"Email" => "Email",
+"Request verification link" => "solicitar um link de confirmação",
+"Please re-enter a valid email address" => "Por favor digitar novamente um endereço de correio válido",
+"You will receive an email with verification link" => "Você receberá uma mensagem de correio electrónico com um link para verificação da sua conta"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
