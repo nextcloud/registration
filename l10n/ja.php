3c7b318f6eb9a8ea0d000f4cc9b896e1bc4a44f6
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-12 03:30:18 -0400

    [tx-robot] updated from transifex

---
+++ l10n/ja.php
@@ -0,0 +1,17 @@
+<?php
+$TRANSLATIONS = array(
+"Email address you entered is not valid" => "入力されたメールアドレスが正しくありません",
+"There is already a pending registration with this email" => "このメールアドレスで登録中断しているものがあります。",
+"Verify your ownCloud registration request" => "ownCloud 登録リクエストを確認してください",
+"Invalid verification URL. No registration request with this verification URL is found." => "確認URLが不正です。この確認URLに紐付く登録リクエストは、ありません。",
+"To create a new account on ownCloud, just click the following link: {link}" => "ownCloud に新しいアカウントを作成する場合、以下のリンクをクリックしてください: {link}",
+"Username" => "ユーザーID",
+"Password" => "パスワード",
+"Create account" => "アカウントを作成",
+"Thank you for registering, you should receive verification link in a few minutes." => "ご登録ありがとうございました。数分以内に確認URLをお送りします。",
+"Email" => "メール",
+"Request verification link" => "確認URLリンクをリクエスト",
+"Please re-enter a valid email address" => "有効なメールアドレスを再度入力してください。",
+"You will receive an email with verification link" => "確認URLの入ったメールをお送り致します。"
+);
+$PLURAL_FORMS = "nplurals=1; plural=0;";
