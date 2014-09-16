b9f117d0e808b5bea58b9abcc2675b291da8f032
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-16 00:45:24 -0400

    [tx-robot] updated from transifex

--- l10n/el.php
+++ l10n/el.php
@@ -1,7 +1,9 @@
 <?php
 $TRANSLATIONS = array(
 "Email address you entered is not valid" => "Η ηλεκτρονική διεύθυνση που εισάγατε δεν είναι έγκυρη",
+"There is already a pending registration with this email" => "Εκκρεμεί ήδη μία εγγραφή με αυτό το email",
 "Verify your ownCloud registration request" => "Επιβεβαιώστε την αίτηση εγγραφής στο ownCloud",
+"Invalid verification URL. No registration request with this verification URL is found." => "Μη έγκυρο URL επιβεβαίωσης. Καμία αίτηση εγγραφής δε βρέθηκε με αυτό το URL επιβεβαίωσης.",
 "To create a new account on ownCloud, just click the following link: {link}" => "Για να δημιουργήσετε ενα νέο λογριασμό στο ownCloud, κάντε κλικ στον ακόλουθο σύνδεσμο: {link}",
 "Username" => "Όνομα χρήστη",
 "Password" => "Κωδικός πρόσβασης",
