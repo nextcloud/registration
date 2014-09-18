b7c2ad3f86da2243137742c6957bee4cd62768c9
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-18 00:28:10 -0400

    [tx-robot] updated from transifex

--- l10n/el.php
+++ l10n/el.php
@@ -1,17 +1,17 @@
 <?php
 $TRANSLATIONS = array(
 "Email address you entered is not valid" => "Η ηλεκτρονική διεύθυνση που εισάγατε δεν είναι έγκυρη",
-"There is already a pending registration with this email" => "Εκκρεμεί ήδη μία εγγραφή με αυτό το email",
+"There is already a pending registration with this email" => "Εκκρεμεί ήδη μία εγγραφή με αυτή τη διεύθυνση ηλ. ταχυδρομείου",
 "Verify your ownCloud registration request" => "Επιβεβαιώστε την αίτηση εγγραφής στο ownCloud",
-"Invalid verification URL. No registration request with this verification URL is found." => "Μη έγκυρο URL επιβεβαίωσης. Καμία αίτηση εγγραφής δε βρέθηκε με αυτό το URL επιβεβαίωσης.",
-"To create a new account on ownCloud, just click the following link: {link}" => "Για να δημιουργήσετε ενα νέο λογριασμό στο ownCloud, κάντε κλικ στον ακόλουθο σύνδεσμο: {link}",
+"Invalid verification URL. No registration request with this verification URL is found." => "Μη έγκυρη URL επιβεβαίωσης. Καμμία αίτηση εγγραφής δεν βρέθηκε με αυτό το URL επιβεβαίωσης.",
+"To create a new account on ownCloud, just click the following link: {link}" => "Για να δημιουργήσετε ένα νέο λογριασμό στο ownCloud, κάντε κλικ στον ακόλουθο σύνδεσμο: {link}",
 "Username" => "Όνομα χρήστη",
 "Password" => "Κωδικός πρόσβασης",
 "Create account" => "Δημιουργία λογαριασμού",
-"Thank you for registering, you should receive verification link in a few minutes." => "Ευχαριστούμε για την εγγραφή, θα λάβετε τον σύνδεσμο επιβεβαίωσης σε λίγα λεπτά.",
-"Email" => "Ηλεκτρονικό ταχυδρομείο",
+"Thank you for registering, you should receive verification link in a few minutes." => "Ευχαριστούμε για την εγγραφή, θα λάβετε το σύνδεσμο επιβεβαίωσης σε λίγα λεπτά.",
+"Email" => "Ηλ. ταχυδρομείο",
 "Request verification link" => "Αίτηση συνδέσμου επιβεβαίωσης",
-"Please re-enter a valid email address" => "Παρακαλώ εισάγετε ξανά μια έγκυρη ηλεκτρονική διεύθυνση",
-"You will receive an email with verification link" => "Θα λάβετε ένα μήνυμα με τον σύνδεσμο επιβεβαίωσης"
+"Please re-enter a valid email address" => "Παρακαλώ εισάγετε ξανά μια έγκυρη διεύθυνση ηλ. ταχυδρομείου",
+"You will receive an email with verification link" => "Θα λάβετε ένα μήνυμα ηλ. ταχυδρομείου με το σύνδεσμο επιβεβαίωσης"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
---
+++ l10n/sl.php
@@ -0,0 +1,17 @@
+<?php
+$TRANSLATIONS = array(
+"Email address you entered is not valid" => "Vpisan naslov elektronske pošte ni veljaven",
+"There is already a pending registration with this email" => "Za ta naslov elektronske pošte je postopek registracije že v teku.",
+"Verify your ownCloud registration request" => "Overite registracijo ownCloud.",
+"Invalid verification URL. No registration request with this verification URL is found." => "Neveljaven naslov URL overitve. Na tem naslovu ni ustrezne zahteve registracije.",
+"To create a new account on ownCloud, just click the following link: {link}" => "Za ustvarjanje novega računa ownCloud, kliknite na povezavo: {link}",
+"Username" => "Uporabniško ime",
+"Password" => "Geslo",
+"Create account" => "Ustvari račun",
+"Thank you for registering, you should receive verification link in a few minutes." => "Registracija je skoraj končana. V nekaj minutah boste na elektronski naslov prejeli povezavo za overitev.",
+"Email" => "Elektronski naslov",
+"Request verification link" => "Zahtevaj povezavo za overitev",
+"Please re-enter a valid email address" => "Vpišite veljaven naslov elektronske pošte.",
+"You will receive an email with verification link" => "Prejeli boste elektronsko sporočilo s povezavo za overitev."
+);
+$PLURAL_FORMS = "nplurals=4; plural=(n%100==1 ? 0 : n%100==2 ? 1 : n%100==3 || n%100==4 ? 2 : 3);";
