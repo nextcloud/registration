2b7e9db6187a8c27c4357adadbb3c711c6178bb4
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-23 01:45:27 -0400

    [tx-robot] updated from transifex

--- l10n/fr.php
+++ l10n/fr.php
@@ -10,6 +10,7 @@ $TRANSLATIONS = array(
 "Create account" => "Créer un compte",
 "Thank you for registering, you should receive verification link in a few minutes." => "Merci pour votre inscription, vous allez recevoir un lien de vérification dans quelques instants.",
 "Email" => "Adresse courriel",
+"Request verification link" => "Lien de vérification de la requête",
 "Please re-enter a valid email address" => "Veuillez remplir une adresse courriel valide",
 "You will receive an email with verification link" => "Vous allez recevoir un courriel avec un lien de vérification"
 );
