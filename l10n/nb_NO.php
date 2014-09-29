5ade55301fecd90fe93fd234473d6173b45b6554
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-29 00:28:11 -0400

    [tx-robot] updated from transifex

---
+++ l10n/nb_NO.php
@@ -0,0 +1,17 @@
+<?php
+$TRANSLATIONS = array(
+"Email address you entered is not valid" => "E-postadressen som du la enn er ikke gyldig",
+"There is already a pending registration with this email" => "Det finnes allerede en ventende registrering med denne e-postadressen",
+"Verify your ownCloud registration request" => "Verifiser din forespørsel om ownCloud-registrering",
+"Invalid verification URL. No registration request with this verification URL is found." => "Ugyldig URL for verifisering. Ingen registreringsforespørsel ble funnet med denne URL",
+"To create a new account on ownCloud, just click the following link: {link}" => "Klikk denne lenken for å opprette en ny konto på ownCloud: {link}",
+"Username" => "Brukernavn",
+"Password" => "Passord",
+"Create account" => "Opprett konto",
+"Thank you for registering, you should receive verification link in a few minutes." => "Takk for at du registrerte deg. Du vil motta en verifiseringslenke om noen minutter.",
+"Email" => "Epost",
+"Request verification link" => "Be om verifiseringslenke",
+"Please re-enter a valid email address" => "Vennligst angi en gyldig e-postadresse",
+"You will receive an email with verification link" => "Du vil motta en e-post med verifiseringslenke"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
