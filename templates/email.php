c162bbb22889c972e8a124cc19216a3a6b40c4bc
Pellaeon Lin <nfsmwlin@gmail.com>
2014-10-03 16:00:07 +0800

    Email template: new line before link

--- templates/email.php
+++ templates/email.php
@@ -1,2 +1,2 @@
 <?php
-echo str_replace('{link}', $_['link'], $l->t('To create a new account on ownCloud, just click the following link: {link}'));
+echo str_replace('{link}', $_['link'], $l->t('To create a new account on ownCloud, just click the following link:<br/>{link}'));
