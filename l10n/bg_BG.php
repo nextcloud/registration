096fd5909605bc17d720f9473ceec8378c9fa7cb
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-10-09 00:28:10 -0400

    [tx-robot] updated from transifex

--- l10n/bg_BG.php
+++ l10n/bg_BG.php
@@ -1,18 +1,20 @@
 <?php
 $TRANSLATIONS = array(
-"Email address you entered is not valid" => "Имейл адресът, който въведе не е валиден",
+"Email address you entered is not valid" => "Имейл адресът, който въведохте, е невалиден",
 "There is already a pending registration with this email" => "Вече съществува регистрация в режим на изчакване със същия имейл",
-"Verify your ownCloud registration request" => "Потвърди своята ownCloud молба за регистрация",
-"Verification email successfully sent." => "Имейл за проверка успешно изпратен.",
-"Invalid verification URL. No registration request with this verification URL is found." => "Невалидна връзка за потвърждение. Не е намерена молба за регистрация, която да е за тази връзка.",
-"To create a new account on ownCloud, just click the following link:<br/>{link}" => "За да създадеш нов профил в ownCloud, следвай следната връзка:<br/>{link}",
+"Verify your ownCloud registration request" => "Потвърдете своята ownCloud заявка за регистрация",
+"Verification email successfully sent." => "Имейлът за проверка е изпратен успешно.",
+"Invalid verification URL. No registration request with this verification URL is found." => "Невалидна връзка за потвърждение. Не е намерена заявка за регистрация, която да е за тази връзка.",
+"Unable to create user, there are problems with user backend." => "Не може да се създаде потребител, има проблем с администрирането на потребителите.",
+"To create a new account on ownCloud, just click the following link:<br/>{link}" => "За да създадете нов профил в ownCloud, последвайте връзката:<br/>{link}",
+"Welcome, you can create your account below." => "Добре дошли, можете да си създадете профил по-долу.",
 "Username" => "Потребителско Име",
 "Password" => "Парола",
 "Create account" => "Създаване на профил",
-"Thank you for registering, you should receive verification link in a few minutes." => "Благодаря за регистрирането, би трябвало да получиш връзка за потвърждение в следващите няколко минути.",
+"Thank you for registering, you should receive verification link in a few minutes." => "Благодаря за регистрирането, би трябвало да получите връзка за потвърждение в следващите няколко минути.",
 "Email" => "Имейл",
-"Request verification link" => "Поискай връзка за потвърждение",
-"Please re-enter a valid email address" => "Моля, въведи валиден имейл повторно",
-"You will receive an email with verification link" => "Ще получиш имейл с връзка за потвърждаване"
+"Request verification link" => "Поискайте връзка за потвърждение",
+"Please re-enter a valid email address" => "Моля, въведете отново валиден имейл",
+"You will receive an email with verification link" => "Ще получите имейл с връзка за потвърждение"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
