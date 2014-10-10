33ecf80167778ed003fe1491b8fd919ec3776285
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-10-10 00:28:10 -0400

    [tx-robot] updated from transifex

--- l10n/de_DE.php
+++ l10n/de_DE.php
@@ -2,19 +2,19 @@
 $TRANSLATIONS = array(
 "Email address you entered is not valid" => "Die eingegebene E-Mail-Adresse ist ungültig.",
 "There is already a pending registration with this email" => "Es gibt bereits eine Registrierung für diese E-Mail-Adresse.",
-"Verify your ownCloud registration request" => "Bestätigen Sie Ihre ownCloud-Registrierung.",
-"Verification email successfully sent." => "E-Mail zur Verifizierung erfolgreich gesendet.",
-"Invalid verification URL. No registration request with this verification URL is found." => "Ungültige Bestätigung-URL. Keine Registrierung mit dieser Bestätigung-URL gefunden.",
-"Unable to create user, there are problems with user backend." => "Anlegen des Nutzer ist nicht möglich, es gibt Probleme mit dem Nutzer-Backend.",
+"Verify your ownCloud registration request" => "Bitte Ihre ownCloud-Registrierung bestätigen.",
+"Verification email successfully sent." => "E-Mail zur Bestätigung erfolgreich gesendet.",
+"Invalid verification URL. No registration request with this verification URL is found." => "Ungültige Bestätigungsadresse. Keine Registrierunganfrage mit dieser Bestätigungsadresse gefunden.",
+"Unable to create user, there are problems with user backend." => "Benutzer konnte nicht erstellt werden, es gibt Probleme mit dem Benutzerhintergrundprogramm.",
 "To create a new account on ownCloud, just click the following link:<br/>{link}" => "Zur Erstellung eines neuen ownCloud-Kontos klicken Sie bitte auf den nachfolgenden Link:<br/>{link}",
 "Welcome, you can create your account below." => "Willkommen, Sie können im unteren Teil Ihr Konto anlegen.",
 "Username" => "Benutzername",
 "Password" => "Passwort",
-"Create account" => "Account erstellen",
+"Create account" => "Konto erstellen",
 "Thank you for registering, you should receive verification link in a few minutes." => "Danke für Ihre Registrierung. Sie sollten einen Bestätigungslink in wenigen Minuten erhalten.",
 "Email" => "E-Mail",
 "Request verification link" => "Bestätigungslink anfragen",
 "Please re-enter a valid email address" => "Bitte nochmals eine gültige E-Mail-Adresse angeben",
-"You will receive an email with verification link" => "Siewerden eine E-Mail mit einem Bestätigungslink erhalten"
+"You will receive an email with verification link" => "Sie werden eine E-Mail mit einem Bestätigungslink erhalten"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
