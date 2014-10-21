f17129495385807e579633839787c8589d65081e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-10-21 00:28:15 -0400

    [tx-robot] updated from transifex

--- l10n/pt_PT.php
+++ l10n/pt_PT.php
@@ -1,7 +1,14 @@
 <?php
 $TRANSLATIONS = array(
 "Email address you entered is not valid" => "O endereço de correio electrónico introduzido é inválido",
+"There is already a pending registration with this email" => "Já existe um registo pendente com este correio electrónico",
 "Verify your ownCloud registration request" => "Verifique o seu pedido de registo de ownCloud",
+"Verification email successfully sent." => "Email de verificação enviado com sucesso.",
+"Invalid verification URL. No registration request with this verification URL is found." => "URL de verificação inválido. Não existe nenhum pedido de registo com este URL.",
+"Unable to create user, there are problems with user backend." => "Não é possível criar utilizador, existem problemas com o painel de utilizador.",
+"Your account has been successfully created, you can <a href=\"{link}\">log in now</a>." => "Conta criada com sucesso, podes <a href=\"{link}\">entrar agora</a>.",
+"To create a new account on ownCloud, just click the following link:<br/>{link}" => "Para criar uma conta nova na ownCloud, clica no seguinte link:<br/>{link}",
+"Welcome, you can create your account below." => "Bem vind@, podes criar a tua conta abaixo.",
 "Username" => "Nome de utilizador",
 "Password" => "Senha",
 "Create account" => "Criar conta",
