708016b094ad950ba8b48d7a677baacdd51119b2
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-11-02 00:28:12 -0400

    [tx-robot] updated from transifex

--- l10n/ja.php
+++ l10n/ja.php
@@ -6,6 +6,7 @@ $TRANSLATIONS = array(
 "Verification email successfully sent." => "確認メールを送信しました。",
 "Invalid verification URL. No registration request with this verification URL is found." => "確認URLが不正です。この確認URLに紐付く登録リクエストは、ありません。",
 "Unable to create user, there are problems with user backend." => "ユーザーを作成出来ません。バックエンドのユーザー管理システムに問題があります。",
+"Your account has been successfully created, you can <a href=\"{link}\">log in now</a>." => "アカウントは無事作成されました。 こちらのリンクから<a href=\"{link}\">ログイン</a>してください。",
 "To create a new account on ownCloud, just click the following link:\n{link}" => "ownCloud に新しいアカウントを作成するには、以下のリンクをクリックしてください: {link}",
 "Welcome, you can create your account below." => "ようこそ、以下から新しくアカウントを作成できます。",
 "Username" => "ユーザーID",
