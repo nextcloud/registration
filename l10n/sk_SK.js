a8884a04c087635a1f0d3962fc7cb1cdd4665a4e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-11-06 00:28:13 -0500

    [tx-robot] updated from transifex

---
+++ l10n/sk_SK.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "registration",
+    {
+    "Email address you entered is not valid" : "Zadaná emailová adresa nie je správna",
+    "There is already a pending registration with this email" : "Registrácia s touto emailovou adresou už čaká na overenie registrácie",
+    "Verify your ownCloud registration request" : "Overte svoju požiadavku na registráciu do ownCloud",
+    "Verification email successfully sent." : "Overovací email bol úspešne odoslaný.",
+    "Invalid verification URL. No registration request with this verification URL is found." : "Neplatná overovacia URL. Nebola nájdená žiadna požiadavka o registráciu pre túto overovaciu URL.",
+    "Unable to create user, there are problems with user backend." : "Nedá sa vytvoriť používateľský účet. Nastala chyba v používateľskom backende.",
+    "Your account has been successfully created, you can <a href=\"{link}\">log in now</a>." : "Váš účet bol vytvorený, teraz sa môžete <a href=\"{link}\">prihlásiť</a>.",
+    "To create a new account on ownCloud, just click the following link:\n{link}" : "Pre vytvorenie nového účtu kliknite na nasledovný odkaz:\n{link}",
+    "Welcome, you can create your account below." : "Vitajte. Účet si môžete založiť nižšie.",
+    "Username" : "Používateľské meno",
+    "Password" : "Heslo",
+    "Create account" : "Vytvoriť účet",
+    "Thank you for registering, you should receive verification link in a few minutes." : "Ďakujeme za vašu registráciu. V priebehu niekoľkých minút by ste mali dostať emailom overovací odkaz.",
+    "Email" : "Email",
+    "Request verification link" : "Vyžiadať overovací odkaz",
+    "Please re-enter a valid email address" : "Zadajte znovu platnú emailovú adresu",
+    "You will receive an email with verification link" : "Dostanete email s overovacím odkazom"
+},
+"nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;");
