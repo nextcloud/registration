a7227d861feec54572621408497515f1d849a107
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-12-23 00:28:12 -0500

    [tx-robot] updated from transifex

---
+++ l10n/mn.js
@@ -0,0 +1,18 @@
+OC.L10N.register(
+    "registration",
+    {
+    "Email address you entered is not valid" : "Таны оруулсан и-мэйл хаяг буруу байна",
+    "There is already a pending registration with this email" : "Энэ и-мэйл хаягаар бүртгэл аль хэдийнэ явагдаж байна",
+    "Verify your ownCloud registration request" : "Та өөрийн бүртгэлийн хүсэлтээ баталгаажуулна уу",
+    "Verification email successfully sent." : "Баталгаажилтын мэйл амжилттай илгээгдлээ",
+    "Your account has been successfully created, you can <a href=\"{link}\">log in now</a>." : "Таны аккаунт амжилттай үүсгэгдлээ, та одоо <a href=\"{link}\">нэвтэрч болно</a>.",
+    "Username" : "Хэрэглэгчийн нэр",
+    "Password" : "Нууц үг",
+    "Create account" : "Аккаунт үүсгэх",
+    "Thank you for registering, you should receive verification link in a few minutes." : "Манайд бүртгүүлсэнд баярлалаа, та удахгүй баталгаажилтын мэйл хүлээн авах болно.",
+    "Email" : "И-мэйл",
+    "Request verification link" : "Баталгаажилтын холбоос хүсэх",
+    "Please re-enter a valid email address" : "Зөв и-мэйл хаягаа дахин оруулна уу",
+    "You will receive an email with verification link" : "Та баталгаажилтын холбоостой и-мэйл хүлээн авна"
+},
+"nplurals=2; plural=(n != 1);");
