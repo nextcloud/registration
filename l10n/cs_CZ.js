76df0135add81cb67a2a93c6c4c1152095d9c835
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-12-24 00:28:12 -0500

    [tx-robot] updated from transifex

--- l10n/cs_CZ.js
+++ l10n/cs_CZ.js
@@ -1,10 +1,10 @@
 OC.L10N.register(
     "registration",
     {
-    "Email address you entered is not valid" : "Zadaná e-mailová adresa není správná",
-    "There is already a pending registration with this email" : "Již existuje čekající registrace s touto e-mailovou adresou",
+    "Email address you entered is not valid" : "Zadaná emailová adresa není správná",
+    "There is already a pending registration with this email" : "Již existuje čekající registrace s touto emailovou adresou",
     "Verify your ownCloud registration request" : "Ověřte svůj požadavek na registraci ownCloud",
-    "Verification email successfully sent." : "Ověřovací e-mail úspěšně odeslán.",
+    "Verification email successfully sent." : "Ověřovací email úspěšně odeslán.",
     "Invalid verification URL. No registration request with this verification URL is found." : "Neplatná ověřovací URL. Nebyl nalezen žádný registrační požadavek pro tuto ověřovací URL.",
     "Unable to create user, there are problems with user backend." : "Není možné vytvořit uživatelský účet, nastaly potíže v systému úložiště uživatelů.",
     "Your account has been successfully created, you can <a href=\"{link}\">log in now</a>." : "Váš účet byl úspěšně vytvořen, můžete se nyní <a href=\"{link}\">přihlásit</a>.",
@@ -14,9 +14,9 @@ OC.L10N.register(
     "Password" : "Heslo",
     "Create account" : "Vytvořit účet",
     "Thank you for registering, you should receive verification link in a few minutes." : "Děkujeme za vaši registraci, během několika minut byste měli obdržet ověřovací odkaz.",
-    "Email" : "E-mail",
+    "Email" : "Email",
     "Request verification link" : "Vyžádat ověřovací odkaz",
-    "Please re-enter a valid email address" : "Zadejte znovu platnou e-mailovou adresu",
-    "You will receive an email with verification link" : "Obdržíte e-mail s ověřovacím odkazem"
+    "Please re-enter a valid email address" : "Zadejte znovu platnou emailovou adresu",
+    "You will receive an email with verification link" : "Obdržíte email s ověřovacím odkazem"
 },
 "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;");
