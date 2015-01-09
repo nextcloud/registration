b970766c7e24cc1f9d17ba7a17783c962337e881
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-09 00:28:13 -0500

    [tx-robot] updated from transifex

---
+++ l10n/pl.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "registration",
+    {
+    "Email address you entered is not valid" : "Adres email, który wprowadziłeś jest nieprawidłowy.",
+    "There is already a pending registration with this email" : "Istnieje już rejestracja w toku z tego adresu email",
+    "Verify your ownCloud registration request" : "Zweryfikuj swoje żądanie rejestracji ownCloud",
+    "Verification email successfully sent." : "Pomyślnie wysłano email weryfikacyjny.",
+    "Invalid verification URL. No registration request with this verification URL is found." : "Nieprawidłowy URL weryfikacji. Nie ma żadnego żądania weryfikacji pod tym URL-em.",
+    "Unable to create user, there are problems with user backend." : "Nie można utworzyć użytkownika, wystąpiły problemy z modułem użytkowników.",
+    "Your account has been successfully created, you can <a href=\"{link}\">log in now</a>." : "Twoje konto zostało pomyślnie utworzone, możesz się teraz<a href=\"{link}\">zalogować</a>.",
+    "To create a new account on ownCloud, just click the following link:\n{link}" : "Aby utworzyć nowe konto na ownCloud, po prostu kliknij następujący link:\n{link}",
+    "Welcome, you can create your account below." : "Witaj, możesz utworzyć swoje konto poniżej.",
+    "Username" : "Nazwa użytkownika",
+    "Password" : "Hasło",
+    "Create account" : "Utwórz konto",
+    "Thank you for registering, you should receive verification link in a few minutes." : "Dziękujemy za rejestrację, powinieneś otrzymać link weryfikacyjny w ciągu kilku minut.",
+    "Email" : "Email",
+    "Request verification link" : "Zażądaj linku weryfikacyjnego",
+    "Please re-enter a valid email address" : "Proszę ponownie wprowadzić adres email",
+    "You will receive an email with verification link" : "Otrzymasz email z linkiem weryfikacyjnym"
+},
+"nplurals=3; plural=(n==1 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
