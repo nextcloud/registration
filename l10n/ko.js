f68255398de4ddd0f00419d3708e99f026a17aa3
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-17 00:28:13 -0500

    [tx-robot] updated from transifex

---
+++ l10n/ko.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "registration",
+    {
+    "Email address you entered is not valid" : "입력한 이메일 주소가 올바르지 않음",
+    "There is already a pending registration with this email" : "입력한 이메일 주소로 가입 대기 중",
+    "Verify your ownCloud registration request" : "ownCloud 가입 요청 확인",
+    "Verification email successfully sent." : "확인 이메일을 보냈습니다.",
+    "Invalid verification URL. No registration request with this verification URL is found." : "확인 URL이 잘못되었습니다. 이 확인 URL과 연결된 가입 요청이 없습니다.",
+    "Unable to create user, there are problems with user backend." : "사용자 백엔드 오류로 사용자를 등록할 수 없습니다.",
+    "Your account has been successfully created, you can <a href=\"{link}\">log in now</a>." : "회원 가입이 완료되었으며 <a href=\"{link}\">로그인할 수 있습니다</a>.",
+    "To create a new account on ownCloud, just click the following link:\n{link}" : "ownCloud에 새로 가입하려면 다음 링크를 누르십시오:\n{link}",
+    "Welcome, you can create your account below." : "안녕하세요, 아래에서 회원 가입할 수 있습니다.",
+    "Username" : "사용자 이름",
+    "Password" : "암호",
+    "Create account" : "계정 만들기",
+    "Thank you for registering, you should receive verification link in a few minutes." : "등록해 주셔서 감사합니다. 잠시 후 확인 링크를 받을 수 있습니다.",
+    "Email" : "이메일",
+    "Request verification link" : "확인 링크 요청",
+    "Please re-enter a valid email address" : "올바른 이메일 주소를 다시 입력해 주십시오",
+    "You will receive an email with verification link" : "확인 링크가 포함된 이메일을 받을 것입니다"
+},
+"nplurals=1; plural=0;");
