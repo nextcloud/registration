15879a33d124ada73eaf2a3240df991c03c6ee93
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-07 00:28:15 -0500

    [tx-robot] updated from transifex

--- l10n/ru.js
+++ l10n/ru.js
@@ -1,22 +1,22 @@
 OC.L10N.register(
     "registration",
     {
-    "Email address you entered is not valid" : "Указанный почтовый адрес недействителен",
-    "There is already a pending registration with this email" : "Уже есть запрос на регистрацию с этим почтовым адресом.",
+    "Email address you entered is not valid" : "Указанный вами адрес email недействителен",
+    "There is already a pending registration with this email" : "Уже есть запрос на регистрацию с этим адресом email.",
     "Verify your ownCloud registration request" : "Подтвердите запрос на регистрацию в ownCloud",
-    "Verification email successfully sent." : "Проверочное сообщение успешно отправлено.",
+    "Verification email successfully sent." : "Проверочное письмо успешно отправлено.",
     "Invalid verification URL. No registration request with this verification URL is found." : "Недействительный проверочный URL. Запросов на регистрацию с таким проверочным URL не найдено.",
-    "Unable to create user, there are problems with user backend." : "Невозможно создать учетную запись, возникли проблемы с пользовательским бэкендом.",
+    "Unable to create user, there are problems with user backend." : "Невозможно создать учетную запись, возникли проблемы с пользовательской частью.",
     "Your account has been successfully created, you can <a href=\"{link}\">log in now</a>." : "Учетная запись успешно создана, теперь вы можете <a href=\"{link}\">войти на сайт</a>.",
     "To create a new account on ownCloud, just click the following link:\n{link}" : "Чтобы создать новую учётную запись в ownCloud, перейдите по следующей ссылке:\n{link}",
     "Welcome, you can create your account below." : "Добро пожаловать, Вы можете создать учетную запись ниже.",
     "Username" : "Имя пользователя",
     "Password" : "Пароль",
     "Create account" : "Создать учётную запись",
-    "Thank you for registering, you should receive verification link in a few minutes." : "Спасибо за регистрацию, вы должны получить подтверждающую ссылку через несколько минут.",
+    "Thank you for registering, you should receive verification link in a few minutes." : "Спасибо за регистрацию, через несколько минут вы должны получить проверочную ссылку.",
     "Email" : "Email",
     "Request verification link" : "Запросить проверочную ссылку",
-    "Please re-enter a valid email address" : "Введите повторно действительный адрес электронной почты.",
-    "You will receive an email with verification link" : "Вы получите сообщение с проверочной ссылкой."
+    "Please re-enter a valid email address" : "Пожалуйста введите повторно действительный адрес email.",
+    "You will receive an email with verification link" : "Вы получите письмо с проверочной ссылкой."
 },
 "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
