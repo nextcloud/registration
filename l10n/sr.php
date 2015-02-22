662d28936ced8490539c881b67621ed4977dbd71
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-22 00:28:16 -0500

    [tx-robot] updated from transifex

---
+++ l10n/sr.php
@@ -0,0 +1,21 @@
+<?php
+$TRANSLATIONS = array(
+"Email address you entered is not valid" => "Е-адреса коју сте унели није исправна",
+"There is already a pending registration with this email" => "Већ постоји започет поступак регистрације са том е-адресом",
+"Verify your ownCloud registration request" => "Потврдите свој захтев за ОунКлауд регистрацију ",
+"Verification email successfully sent." => "Верификациона е-пошта је успешно послата",
+"Invalid verification URL. No registration request with this verification URL is found." => "Неисправан верификациони УРЛ. Ниједан захтев за регистрацију са овим URL није пронађен.",
+"Unable to create user, there are problems with user backend." => "Не могу да створим корисника. Постоји проблем са позадином за корисника.",
+"Your account has been successfully created, you can <a href=\"{link}\">log in now</a>." => "Ваш налог је успешно створен. Можете се <a href=\"{link}\">пријавити сада</a>.",
+"To create a new account on ownCloud, just click the following link:\n{link}" => "Да направите нови налог на ОунКлауду, кликните на следећу везу:\n{link}",
+"Welcome, you can create your account below." => "Добро дошли. Испод можете направити ваш налог.",
+"Username" => "Корисничко име",
+"Password" => "Лозинка",
+"Create account" => "Направи налог",
+"Thank you for registering, you should receive verification link in a few minutes." => "Хвала на регистрацији. Требало би да добијете верификациону везу за неколико минута.",
+"Email" => "Е-пошта",
+"Request verification link" => "Затражите верификациону везу",
+"Please re-enter a valid email address" => "Унесите поново исправну е-адресу",
+"You will receive an email with verification link" => "Примићете е-пошту са верификационом везом"
+);
+$PLURAL_FORMS = "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);";
