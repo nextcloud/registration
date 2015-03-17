bb0e12d80ba0877c313670f9b3fda204bf9f9f8e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-03-17 00:28:16 -0400

    [tx-robot] updated from transifex

--- l10n/sr.js
+++ l10n/sr.js
@@ -4,8 +4,8 @@ OC.L10N.register(
     "Email address you entered is not valid" : "Е-адреса коју сте унели није исправна",
     "There is already a pending registration with this email" : "Већ постоји започет поступак регистрације са том е-адресом",
     "Verify your ownCloud registration request" : "Потврдите свој захтев за ОунКлауд регистрацију ",
-    "Verification email successfully sent." : "Верификациона е-пошта је успешно послата",
-    "Invalid verification URL. No registration request with this verification URL is found." : "Неисправан верификациони УРЛ. Ниједан захтев за регистрацију са овим URL није пронађен.",
+    "Verification email successfully sent." : "Порука за потврду је успешно послата",
+    "Invalid verification URL. No registration request with this verification URL is found." : "Неисправан УРЛ за потврду. Ниједан захтев за регистрацију са овим УРЛ-ом није пронађен.",
     "Unable to create user, there are problems with user backend." : "Не могу да створим корисника. Постоји проблем са позадином за корисника.",
     "Your account has been successfully created, you can <a href=\"{link}\">log in now</a>." : "Ваш налог је успешно створен. Можете се <a href=\"{link}\">пријавити сада</a>.",
     "To create a new account on ownCloud, just click the following link:\n{link}" : "Да направите нови налог на ОунКлауду, кликните на следећу везу:\n{link}",
@@ -13,10 +13,10 @@ OC.L10N.register(
     "Username" : "Корисничко име",
     "Password" : "Лозинка",
     "Create account" : "Направи налог",
-    "Thank you for registering, you should receive verification link in a few minutes." : "Хвала на регистрацији. Требало би да добијете верификациону везу за неколико минута.",
+    "Thank you for registering, you should receive verification link in a few minutes." : "Хвала на регистрацији. Требало би да добијете везу за потврду за неколико минута.",
     "Email" : "Е-пошта",
-    "Request verification link" : "Затражите верификациону везу",
+    "Request verification link" : "Затражите везу за потврду",
     "Please re-enter a valid email address" : "Унесите поново исправну е-адресу",
-    "You will receive an email with verification link" : "Примићете е-пошту са верификационом везом"
+    "You will receive an email with verification link" : "Примићете е-пошту са везом за потврду"
 },
 "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
