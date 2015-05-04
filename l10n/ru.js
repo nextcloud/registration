cfb4e5c5026f8b2d82036e91015be60e3ac42c16
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-05-04 00:28:18 -0400

    [tx-robot] updated from transifex

--- l10n/ru.js
+++ l10n/ru.js
@@ -19,4 +19,4 @@ OC.L10N.register(
     "Please re-enter a valid email address" : "Пожалуйста введите повторно действительный адрес email.",
     "You will receive an email with verification link" : "Вы получите письмо с проверочной ссылкой."
 },
-"nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
+"nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);");
