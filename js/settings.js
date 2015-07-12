cd4a61243a1152d29b13734db1777aa00e7b86e9
Johannes Starosta <j.starosta@tu-braunschweig.de>
2015-07-13 00:49:30 +0200

    changed settings.js to reflect changes in template

--- js/settings.js
+++ js/settings.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 	function saveSettings() {
-		var post = $('#registered_user_group').serialize();
+		var post1 = $('#registered_user_group').serialize();
+		var post2 = $('#allowed_domains').serialize();
 
-		$.post(OC.generateUrl('/apps/registration/settings'), post);
+		$.post(OC.generateUrl('/apps/registration/settings'), post1,post2);
 	}
 
 	$('#registered_user_group').change(saveSettings);
+	$('#allowed_domains').change(saveSettings);
 });
