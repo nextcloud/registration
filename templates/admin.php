321435090d2f400f4c70b67408c925e66d89ebd7
Johannes Starosta <j.starosta@tu-braunschweig.de>
2015-07-19 06:49:05 +0200

    Fixed explaination in settings

--- templates/admin.php
+++ templates/admin.php
@@ -17,8 +17,8 @@ foreach ( $_['groups'] as $group ) {
 	</p>
 	<p>
 	<label for="allowed_domains"><?php p($l->t('Allowed domains for registration')); ?></label>
-	<input type="text" placeholder="<?php p$l->t('"Enter a semicolon-seperated list of allowed domains. Example: owncloud.com;github.com');?>" id="allowed_domains" name="allowed_domains" value=<?php p($_['allowed']);?>>
-	<em><?php p$l->t('"Enter a semicolon-seperated list of allowed domains. Example: owncloud.com;github.com');?> 
+	<input type="text" placeholder="<?php p($l->t('Enter a semicolon-seperated list of allowed domains. Example: owncloud.com;github.com'));?>" id="allowed_domains" name="allowed_domains" value=<?php p($_['allowed']);?>>
+	<em><?php p($l->t('Enter a semicolon-seperated list of allowed domains. Example: owncloud.com;github.com'));?> 
 </em>
 	</p>
 
