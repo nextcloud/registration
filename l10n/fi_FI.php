c51d8d477712815a355d88af3957e8884fc3c967
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-02-06 00:28:11 -0500

    [tx-robot] updated from transifex

--- l10n/fi_FI.php
+++ l10n/fi_FI.php
@@ -1,12 +1,14 @@
 <?php
 $TRANSLATIONS = array(
 "Register" => "Rekisteröidy",
+"The email address you entered is not valid" => "Antamasi sähköpostiosoite ei ole kelvollinen",
 "A problem occurred sending email, please contact your administrator." => "Sähköpostia lähettäessä tapahtui virhe. Ole yhteydessä ylläpitoon.",
 "There is already a pending registration with this email, a new verification email has been sent to the address." => "Tällä sähköpostiosoitteella on jo olemassa oleva rekisteröintipyyntö. Uusi vahvistusviesti on lähetetty sähköpostitse.",
 "A user has already taken this email, maybe you already have an account?" => "Tämä sähköpostiosoite on jo jonkun käyttäjän käytössä. Oletko varma, ettei sinulla ole jo tunnusta tähän ownCloudiin?",
 "You can <a href=\"{login}\">log in now</a>." => "Voit <a href=\"{login}\">kirjautua sisään</a> nyt.",
 "Verification email successfully sent." => "Vahvistussähköposti lähetettiin onnistuneesti.",
 "Invalid verification URL. No registration request with this verification URL is found." => "Virheellinen vahvistuksen verkko-osoite. Kyseisellä osoitteella ei löytynyt rekisteröintipyyntöä.",
+"Unable to create user, there are problems with the user backend." => "Käyttäjän luominen ei onnistu. Ongelmia käyttäjätaustaosan kanssa.",
 "Unable to set user email: " => "Käyttäjän sähköpostin asettaminen epäonnistui:",
 "Failed to delete pending registration request" => "Odottavan rekisteröintipyynnön poistaminen epäonnistui",
 "Your account has been successfully created, you can <a href=\"{link}\">log in now</a>." => "Tilisi on luotu onnistuneesti. Voit <a href=\"{link}\">kirjautua sisään</a> nyt.",
@@ -18,13 +20,17 @@ $TRANSLATIONS = array(
 "Default group that all registered users belong" => "Oletusryhmä, johon kaikki rekisteröityneet käyttäjät kuuluvat",
 "None" => "Ei mitään",
 "Allowed mail address domains for registration" => "Rekisteröintiin sallittujen sähköpostiosoitteiden verkkotunnukset",
+"Enter a semicolon-separated list of allowed domains. Example: owncloud.com;github.com" => "Kirjoita puolipistein eroteltu lista sallituista verkkotunnuksista. Esimerkki: owncloud.com;github.com",
+"Registration is only allowed for the following domains:" => "Rekisteröinti on sallittu vain seuraavista verkkotunnuksista:",
 "To create a new account on %s, just click the following link:" => "Luo uusi %s-tili napsauttamalla seuraavaa linkkiä:",
 "Welcome, you can create your account below." => "Tervetuloa, voit luoda tilisi alta.",
 "Username" => "Käyttäjätunnus",
 "Password" => "Salasana",
 "Create account" => "Luo tili",
+"Thank you for registering, you should receive a verification link in a few minutes." => "Kiitos rekisteröinnistä, saat vahvistuslinkin muutaman minuutin kuluessa.",
 "Email" => "Sähköpostiosoite",
 "Request verification link" => "Pyydä vahvistuslinkkiä",
-"Please re-enter a valid email address" => "Anna uudelleen kelvollinen sähköpostiosoite"
+"Please re-enter a valid email address" => "Anna uudelleen kelvollinen sähköpostiosoite",
+"You will receive an email with a verification link" => "Saat viestin sähköpostitse, joka sisältää vahvistuslinkin"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
--- l10n/pt_PT.php
+++ l10n/pt_PT.php
@@ -1,12 +1,14 @@
 <?php
 $TRANSLATIONS = array(
 "Register" => "Registar",
+"The email address you entered is not valid" => "O endereço de correio eletrónico introduzido não é válido",
 "A problem occurred sending email, please contact your administrator." => "Ocorrei um problema ao enviar a mensagem, por favor, contacte o seu administrador.",
 "There is already a pending registration with this email, a new verification email has been sent to the address." => "Já existe um registo pendente com este endereço de correio eletronico, foi enviado um novo email de verificação para este endereço.",
 "A user has already taken this email, maybe you already have an account?" => "Já existe um utilizador com este e-mail, já poderá ter uma conta?",
 "You can <a href=\"{login}\">log in now</a>." => "Agora, pode <a href=\"{login}\">iniciar a sessão</a>.",
 "Verification email successfully sent." => "Mensagem de verificação enviada com sucesso.",
 "Invalid verification URL. No registration request with this verification URL is found." => "URL de verificação inválido. Não foi encontrado nenhum pedido de registo com este URL de verificação.",
+"Unable to create user, there are problems with the user backend." => "Não é possível criar o utilizador, existem problemas com a interface do utilizador.",
 "Unable to set user email: " => "Não é possível definir o correio eletrónico do utilziador: ",
 "Failed to delete pending registration request" => "Não foi possível eliminar o pedido de registo pendente",
 "Your account has been successfully created, you can <a href=\"{link}\">log in now</a>." => "A sua conta foi criada com sucesso, pode <a href=\"{link}\">iniciar a sessão </a> agora.",
@@ -18,13 +20,17 @@ $TRANSLATIONS = array(
 "Default group that all registered users belong" => "O grupo predefinido em que todos os utilziadores pertencem",
 "None" => "Nenhum",
 "Allowed mail address domains for registration" => "Domínios de correio eletrónico permitidos para registo",
+"Enter a semicolon-separated list of allowed domains. Example: owncloud.com;github.com" => "Introduza uma lista de domínios autorizados separados por ponto e vírgula. Exemplo: owncloud.com;github.com",
+"Registration is only allowed for the following domains:" => "O registo é apenas permitido para os seguintes domínios:",
 "To create a new account on %s, just click the following link:" => "Parar criar uma conta nova %s, clique no seguinte link:",
 "Welcome, you can create your account below." => "Bem-vindo, pode criar a sua conta em baixo.",
 "Username" => "Nome de utilizador",
 "Password" => "Palavra-passe",
 "Create account" => "Criar conta",
+"Thank you for registering, you should receive a verification link in a few minutes." => "Obrigado por se registar, dentro de momentos deverá receber uma hiperligação de verificação.",
 "Email" => "Correio Eletrónico",
 "Request verification link" => "Solicitar hiperligação de verificação",
-"Please re-enter a valid email address" => "Por favor, reinsira um endereço de correio eletrónico válido"
+"Please re-enter a valid email address" => "Por favor, reinsira um endereço de correio eletrónico válido",
+"You will receive an email with a verification link" => "Irá receber uma mensagem com a hiperligação de verificação"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
