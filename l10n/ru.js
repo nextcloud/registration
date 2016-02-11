5e978e3dd713d995113fa4a55ca9df8bd90c58bd
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-02-11 00:28:14 -0500

    [tx-robot] updated from transifex

--- l10n/ru.js
+++ l10n/ru.js
@@ -2,12 +2,14 @@ OC.L10N.register(
     "registration",
     {
     "Register" : "Регистрация",
+    "The email address you entered is not valid" : "Указанный адрес email не является корректным",
     "A problem occurred sending email, please contact your administrator." : "Возникла проблема отправки email, пожалуйста свяжитесь с администратором.",
     "There is already a pending registration with this email, a new verification email has been sent to the address." : "Уже существует запрос на регистрацию с этим адресом email, новое подтверждение было отправлено снова.",
     "A user has already taken this email, maybe you already have an account?" : "Пользователь уже получил этот email, возможно учетная запись уже существует?",
     "You can <a href=\"{login}\">log in now</a>." : "Теперь вы можете <a href=\"{login}\">войти</a>.",
     "Verification email successfully sent." : "Проверочное письмо успешно отправлено.",
     "Invalid verification URL. No registration request with this verification URL is found." : "Недействительный проверочный URL. Запросов на регистрацию с таким проверочным URL не найдено.",
+    "Unable to create user, there are problems with the user backend." : "Ошибка создания пользователя, проблема на стороне клиентской части.",
     "Unable to set user email: " : "Невозможно установить email пользователя:",
     "Failed to delete pending registration request" : "Не удалось удалить существующий запрос на регистрацию",
     "Your account has been successfully created, you can <a href=\"{link}\">log in now</a>." : "Учетная запись успешно создана, теперь вы можете <a href=\"{link}\">войти на сайт</a>.",
@@ -19,13 +21,17 @@ OC.L10N.register(
     "Default group that all registered users belong" : "Группа по умолчанию для всех новых пользователей",
     "None" : "Отсутствует",
     "Allowed mail address domains for registration" : "Допустимые доменны адресов электронной почты  для регистрации",
+    "Enter a semicolon-separated list of allowed domains. Example: owncloud.com;github.com" : "Укажите список разрешенных доменов разделенных точкой с запятой. Например: owncloud.com;github.com",
+    "Registration is only allowed for the following domains:" : "Регистрация доступна только для следующих доменов:",
     "To create a new account on %s, just click the following link:" : "Чтобы создать новую учетную запись на%s, просто нажмите на следующую ссылку:",
     "Welcome, you can create your account below." : "Добро пожаловать, Вы можете создать учетную запись ниже.",
     "Username" : "Имя пользователя",
     "Password" : "Пароль",
     "Create account" : "Создать учётную запись",
+    "Thank you for registering, you should receive a verification link in a few minutes." : "Благодарим за регистрацию, через несколько минут вы получите ссылку для подтверждения.",
     "Email" : "Email",
     "Request verification link" : "Запросить проверочную ссылку",
-    "Please re-enter a valid email address" : "Пожалуйста введите повторно действительный адрес email."
+    "Please re-enter a valid email address" : "Пожалуйста введите повторно действительный адрес email.",
+    "You will receive an email with a verification link" : "Вы получите письмо с ссылкой для подтверждения"
 },
 "nplurals=4; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<12 || n%100>14) ? 1 : n%10==0 || (n%10>=5 && n%10<=9) || (n%100>=11 && n%100<=14)? 2 : 3);");
