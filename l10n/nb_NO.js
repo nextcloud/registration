26cd4f5d18ae2ebefedbfbbbe86d3ec74b3c8d12
ownCloud Jenkins <jenkins@owncloud.com>
2017-09-11 02:36:17 +0000

    [tx-robot] updated from transifex

--- l10n/nb_NO.js
+++ l10n/nb_NO.js
@@ -2,6 +2,7 @@ OC.L10N.register(
     "registration",
     {
     "Register" : "Registrer",
+    "Your registration is pending. Please confirm your email address." : "Registreringen din venter. Vennligst bekreft e-postadressen din.",
     "There is already a pending registration with this email, a new verification email has been sent to the address." : "Det finnes allerede en uavklart registrering med denne e-postadressen. En ny verifiserings-epost er blitt sendt til adressen.",
     "Verification email successfully sent." : "Verifiserings-epost sendt.",
     "Your account has been successfully created, you can <a href=\"%s\">log in now</a>." : "Kontoen din er blitt opprettet. Du kan nå <a href=\"%s\">logge inn</a>. ",
@@ -12,6 +13,7 @@ OC.L10N.register(
     "A problem occurred sending email, please contact your administrator." : "Et problem oppstod ved sending av mailen. Kontakt administratoren.",
     "A new user \"%s\" has created an account on %s" : "En ny bruker \"%s\" har opprettet en konto på %s",
     "A user has already taken this email, maybe you already have an account?" : "En bruker har allerede valgt denne mail-adressen. Kanskje du har en konto allerede?",
+    "You can <a href=\"%s\">log in now</a>." : "Du kan <a href=\"%s\">logge inn nå</a>.",
     "Registration is only allowed for the following domains: " : "Registrering er tillatt kun for følgende domener:",
     "Please provide a valid display name." : "Vennligst oppgi et gyldig brukernavn. ",
     "Please provide a valid user name." : "Vennligst oppgi et gyldig brukernavn. ",
@@ -20,6 +22,7 @@ OC.L10N.register(
     "Unable to create user, there are problems with the user backend." : "Kan ikke opprette bruker; det er problemer med bruker-serveren.",
     "Unable to set user email: " : "Kan ikke sette brukerens e-post:",
     "Failed to delete pending registration request" : "Klarte ikke å slette utestående registreringsforespørsel",
+    "Registration app auto setup" : "Auto-oppsett av registrerings-app",
     "Registration" : "Registrering",
     "Default group that all registered users belong" : "Standard gruppe som alle registrerte brukere er med i",
     "None" : "Ingen",
