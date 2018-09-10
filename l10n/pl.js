c0c0bce97fc15fdb96712487c8ad6750a6ac6321
Marcin Wróblewski <wrbl606@users.noreply.github.com>
2018-09-10 09:10:24 +0200

    "Show password" pl translation

--- l10n/pl.js
+++ l10n/pl.js
@@ -30,6 +30,7 @@ OC.L10N.register(
     "Email" : "Email",
     "Request verification link" : "Zażądaj linku weryfikacyjnego",
     "Please re-enter a valid email address" : "Proszę ponownie wprowadzić adres email",
-    "You will receive an email with a verification link" : "Otrzymasz email z linkiem weryfikacyjnym"
+    "You will receive an email with a verification link" : "Otrzymasz email z linkiem weryfikacyjnym",
+    "Show password": "Pokaż hasło"
 },
 "nplurals=4; plural=(n==1 ? 0 : (n%10>=2 && n%10<=4) && (n%100<12 || n%100>14) ? 1 : n!=1 && (n%10>=0 && n%10<=1) || (n%10>=5 && n%10<=9) || (n%100>=12 && n%100<=14) ? 2 : 3);");
