22e295ab9ef3dcc3126205ad0b5dd438205bb5c1
Marcin Wróblewski <wrbl606@users.noreply.github.com>
2018-09-10 09:08:00 +0200

    Fixed show/hide password toggle at JS side

--- js/form.js
+++ js/form.js
@@ -1,7 +1,14 @@
-$('#show-password').click(function () {
-	if ( $('#password').attr('type') == "password" ) {
-		$('#password').attr('type', 'text');
-	} else {
-		$('#password').attr('type', 'password');
-	}
+// var instead of let/const for better older browsers compatibility
+var passwordTextField;
+function togglePasswordTextFieldVisibility() {
+    if (passwordTextField.attr('type') == "password") {
+        passwordTextField.attr('type', 'text');
+    } else {
+        passwordTextField.attr('type', 'password');
+    }
+}
+
+$(document).ready(function() {
+    passwordTextField = $("#password");
+    $("#show").change(togglePasswordTextFieldVisibility);
 });
