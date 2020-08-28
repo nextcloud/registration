69197003a25e5c55885bd0d55e1f6ba114a52aad
Joas Schilling <coding@schilljs.com>
2020-08-28 17:37:23 +0200

    Cleaner admin settings
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- templates/admin.php
+++ templates/admin.php
@@ -2,37 +2,43 @@
 /** @var array $_ */
 /** @var \OCP\IL10N $l */
 script('registration', 'settings');
+style('registration', 'settings');
 ?>
 <form id="registration_settings_form" class="section">
 	<h2><?php p($l->t('Registration')); ?></h2><span id="registration_settings_msg" class="msg"></span>
+
+	<h3><?php p($l->t('Registered users default group')); ?></h3>
 	<p>
-	<label for="registered_user_group"><?php p($l->t('Default group that all registered users belong')); ?></label>
-	<select id="registered_user_group" name="registered_user_group">
-		<option value="none" <?php echo $_['current'] === 'none' ? 'selected="selected"' : ''; ?>><?php p($l->t('None')); ?></option>
+		<label>
+			<select id="registered_user_group" name="registered_user_group">
+				<option value="none" <?php echo $_['current'] === 'none' ? 'selected="selected"' : ''; ?>><?php p($l->t('None')); ?></option>
 <?php
 foreach ($_['groups'] as $group) {
 	$selected = $_['current'] === $group ? 'selected="selected"' : '';
 	echo '<option value="'.$group.'" '.$selected.'>'.$group.'</option>';
 }
 ?>
-	</select>
-	</p>
-	<p>
-	<label for="allowed_domains"><?php p($l->t('Allowed mail address domains for registration')); ?></label>
-	<input type="text" id="allowed_domains" name="allowed_domains" value=<?php p($_['allowed']);?>>
-	</p>
-	<p>
-	<em><?php p($l->t('Enter a semicolon-separated list of allowed domains. Example: nextcloud.com;example.com'));?></em>
+			</select>
+		</label>
 	</p>
 
-	<div style="margin-top: 10px;">
-		<p>
-			<input type="checkbox" id="admin_approval_required" class="checkbox" name="admin_approval_required" <?php if ($_['approval_required'] === "yes") {
-	echo " checked";
-} ?>>
-			<label for="admin_approval_required"><?php p($l->t('Require admin approval?')); ?></label>
-		</p>
+	<h3><?php p($l->t('Allowed email domains')); ?></h3>
+	<p>
+		<label>
+			<input type="text" id="allowed_domains" name="allowed_domains" value="<?php p($_['allowed']);?>" placeholder="nextcloud.com;*.example.com">
+		</label>
+	</p>
+	<p>
+		<em><?php p($l->t('Enter a semicolon-separated list of allowed domains, * for wildcard. Example: %s', ['nextcloud.com;*.example.com']));?></em>
+	</p>
 
-		<em><?php p($l->t('Enabling "admin approval" will prevent registrations from mobile and desktop clients to complete as the credentials can not be verified by the client until the user was enabled.'));?></em>
-	</div>
+	<h3><?php p($l->t('Admin approval')); ?></h3>
+	<p>
+		<input type="checkbox" id="admin_approval_required" class="checkbox" name="admin_approval_required" <?php if ($_['approval_required'] === "yes") {
+			echo " checked";
+		} ?>>
+		<label for="admin_approval_required"><?php p($l->t('Require admin approval')); ?></label>
+	</p>
+
+	<em><?php p($l->t('Enabling "admin approval" will prevent registrations from mobile and desktop clients to complete as the credentials can not be verified by the client until the user was enabled.'));?></em>
 </form>
