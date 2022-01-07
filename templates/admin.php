fdbc040d79b81b08e8c57826fc692034f1be91f7
rakekniven <2069590+rakekniven@users.noreply.github.com>
2022-01-07 21:48:08 +0100

    l10n: Changed product name to nextcloud
    I wonder why this branch is synced with Transifex ... but it is.
    
    Reported at Transifex.
    
    Signed-off-by: rakekniven <2069590+rakekniven@users.noreply.github.com>

--- templates/admin.php
+++ templates/admin.php
@@ -20,7 +20,7 @@ foreach ($_['groups'] as $group) {
 	<input type="text" id="allowed_domains" name="allowed_domains" value=<?php p($_['allowed']);?>>
 	</p>
 	<p>
-	<em><?php p($l->t('Enter a semicolon-separated list of allowed domains. Example: owncloud.com;github.com'));?></em>
+	<em><?php p($l->t('Enter a semicolon-separated list of allowed domains. Example: nextcloud.com;github.com'));?></em>
 	</p>
 
 	<p>
