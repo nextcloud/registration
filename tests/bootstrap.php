9ceb8533f4581dce2c76a03213e7f77fa725df0e
Thomas Citharel <tcit@tcit.fr>
2025-09-11 18:55:04 +0200

    chore(deps): update deps and put php dev deps in vendor-bin
    Signed-off-by: Thomas Citharel <tcit@tcit.fr>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * SPDX-FileCopyrightText: 2020 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-require_once __DIR__ . '/../../../tests/bootstrap.php';
+if (!defined('PHPUNIT_RUN')) {
+	define('PHPUNIT_RUN', 1);
+}
+
+use OCP\App\IAppManager;
+use OCP\Server;
+
+require_once __DIR__ . '/../../../lib/base.php';
+require_once __DIR__ . '/../../../tests/autoload.php';
 require_once __DIR__ . '/../vendor/autoload.php';
+
+Server::get(IAppManager::class)->loadApp('calendar');
+
+OC_Hook::clear();
